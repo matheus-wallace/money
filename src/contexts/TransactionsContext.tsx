cb1d04ff04e75b20afd4577c7c92af7a4f19755d
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-05-05 20:25:45 -0300

    feat: add axios and refactor of fetch transactions

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from '../lib/axios';
 
 interface Transaction {
   id: number;
@@ -24,16 +25,15 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   async function fetchTransactions(query?: string) {
-    const url = new URL('http://localhost:3333/transactions');
-    if (query) {
-      url.searchParams.append('q', query);
-    }
-    const response = await fetch(url);
+    const response = await api.get('/transactions', {
+      params: {
+        _sort: 'createdAt',
+        _order: 'desc',
+        q: query,
+      },
+    });
 
-    const data = await response.json();
-    console.log(url);
-
-    setTransactions(data);
+    setTransactions(response.data);
   }
 
   useEffect(() => {
